57c6576152248b581cb1300b2b2115745cc1f2a4
Romans Stepanovs <romans.stepanovs@gmail.com>
2014-04-14 19:52:10 +0300

    changes

--- app/controllers/RawDataController.java
+++ app/controllers/RawDataController.java
@@ -15,7 +15,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class Raw extends Controller {
+public class RawDataController extends Controller {
 
 	static RawConnection conn = DBConnectionFactory.getRawDatabase();
 	public static Result getCategories() {
@@ -26,6 +26,6 @@ public class Raw extends Controller {
 
 
     public static Result getViews(String id) {
-        return ok(Arrays.toString(conn.getViews(id)) );
+        return ok(Arrays.toString(conn.getViews(id)));
     }
 }
